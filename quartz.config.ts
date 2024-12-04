65dc6cf26d110a11e8ca14273488280e129bcac2
LostName1 <jonzarmar@gmail.com>
2024-12-04 20:25:12 +0100

    Quartz sync: Dec 4, 2024, 8:25 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "A LA MIERDA SPAM <3",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
